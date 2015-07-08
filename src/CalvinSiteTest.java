c17fa17cccde800fcca6a102ecd73c89cffdd5a3
rossacheson <ross.acheson@gmail.com>
2015-07-08 09:25:05 +0300

    Update w/ first user story

--- src/CalvinSiteTest.java
+++ src/CalvinSiteTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /*
- * As a <role>, i.e. partier
+ * As a <role>
  * I want...
  * So that...
  * @author Ross Acheson
@@ -14,14 +14,59 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  */
 
 //https://github.com/laboon/infsci2955/blob/master/sample_code/RedditTest.java
-public class CalvinSiteTest { 
+public class CalvinSiteTest {
 
 	static WebDriver driver = new HtmlUnitDriver();
-	
+
 	// Start at the home page for Calvin for each test
 	@Before
 	public void setUp() throws Exception {
 		driver.get("http://www.calvin.edu");
+	}
+
+	/*
+	 * As a user I want to see a set of options specific to me on the home page
+	 * So that I can find resources that are relevant to me quickly and easily
+	 * 
+	 * @author Ross Acheson
+	 */
+
+	// Given that I am on the main page
+	// When I view the header
+	// Then I am provided shortcuts for "Students", "Parents", "Alumni",
+	// "Faculty & Staff" and "Admitted Students" links
+	@Test
+	public void testHasShortcutLinks() {
+
+		// Check for the links - if any of
+		// these is not found, fail the test
+
+		try {
+			driver.findElement(By.linkText("Students"));
+			driver.findElement(By.linkText("Parents"));
+			driver.findElement(By.linkText("Alumni"));
+			driver.findElement(By.linkText("Faculty & Staff"));
+			driver.findElement(By.linkText("Admitted Students"));
+		} catch (NoSuchElementException nseex) {
+			fail();
+		}
+	}
+
+	// Given that I am on the alumni page
+	// When I view the page
+	// Then I am provided with a link to support the college
+	@Test
+	public void testHasSupportLink() {
+
+		driver.get("http://www.calvin.edu/alumni/");
+
+		// Check for the link - if not found, fail the test
+
+		try {
+			driver.findElement(By.linkText("Support"));
+		} catch (NoSuchElementException nseex) {
+			fail();
+		}
 	}
 
 	// Given that I am on the main page
@@ -29,41 +74,23 @@ public class CalvinSiteTest {
 	// Then I see that it contains the word "reddit"
 	@Test
 	public void testShowsCorrectTitle() {
-		
+
 		// Simply check that the title contains the word "reddit"
-		
+
 		String title = driver.getTitle();
 		assertTrue(title.contains("reddit"));
 	}
-	
-	// Given that I am on the main page
-	// When I view the header
-	// Then I see that it contains "new", "rising", and "top" links
-	@Test
-	public void testHasCorrectHeaderLinks() {
-		
-		// Check for new, rising, and top links - if any of
-		// these is not found, fail the test
-		
-		try {
-			driver.findElement(By.linkText("new"));
-			driver.findElement(By.linkText("rising"));
-			driver.findElement(By.linkText("top"));
-		} catch (NoSuchElementException nseex) {
-			fail();
-		}
-	}
-	
+
 	// Given that I am on the main page
 	// When I view the Remember Me section
 	// Then I should see that it contains the phrase "remember me"
 	@Test
 	public void testHasRememberMe() {
-		
+
 		// Check that there is a remember-me element
 		// that contains the text "remember me"
 		// If it does not exist, or text is incorrect, fail test
-		
+
 		try {
 			WebElement e = driver.findElement(By.id("remember-me"));
 			String elementText = e.getText();
@@ -72,51 +99,51 @@ public class CalvinSiteTest {
 			fail();
 		}
 	}
-	
+
 	// Given that I am on the main page
 	// When I click on the "new" link
 	// Then I should be redirected to the "new" page
 	@Test
 	public void testSeeNewLinks() {
-		
+
 		// find the "new" link and click on it
 		// The page you go to should include "newest submissions"
 		// in the title
-		
+
 		driver.findElement(By.linkText("new")).click();
 		String newPageTitle = driver.getTitle();
 		assertTrue(newPageTitle.contains("newest submissions"));
 	}
-	
+
 	// Given that I am on the main page
-	// And I am not logged in 
+	// And I am not logged in
 	// When I try to login with an valid username and invalid password
 	// Then I am given the opportunity to reset the password
 	@Test
 	public void testBadPasswordResetLink() {
-		
+
 		// Enter username "meow", password "meow"
-		
+
 		driver.findElement(By.name("user")).sendKeys("meow");
 		driver.findElement(By.name("passwd")).sendKeys("meow");
-		
+
 		// Look for the submit button (in the login div) and click
-		// to attempt to login 
-		
+		// to attempt to login
+
 		WebElement loginDiv = driver.findElement(By.id("login_login-main"));
-		
+
 		WebElement submitButton = loginDiv.findElement(By.className("btn"));
 		submitButton.click();
-		
+
 		// Check that there is a link to reset password and it is visible
-		
+
 		try {
-			WebElement resetPw = driver.findElement(By.linkText("reset password"));
+			WebElement resetPw = driver.findElement(By
+					.linkText("reset password"));
 			assertTrue(resetPw.isDisplayed());
 		} catch (NoSuchElementException nseex) {
 			fail();
 		}
 	}
-	
 
 }
